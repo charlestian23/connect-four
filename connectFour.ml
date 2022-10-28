34a039354d2bef8fde162ef1aa46f5e7c67a4de2
charlestian23 <charles.tian23@gmail.com>
2022-10-27 20:14:29 -0400

    Adjusted the way the board is stored so that each individual list is a column instead of a row

--- connectFour.ml
+++ connectFour.ml
@@ -1,29 +1,40 @@
 let rec create_board rows columns =
-  let rec create_row = function
+  let rec create_column = function
     | 0 -> []
-    | n -> 0 :: create_row (n - 1)
+    | n -> 0 :: create_column (n - 1)
   in
-  let row = create_row columns in
-  match rows with
+  let column = create_column rows in
+  match columns with
   | 0 -> []
-  | n -> row :: create_board (rows - 1) columns
+  | n -> column :: create_board rows (columns - 1)
 
-let rec print_board board =
-  let rec print_row = function
+let rec print_board_grid board row =
+  let rec print_row board row =
+    match board with
     | [] -> print_endline ""
     | h :: t ->
         let _ =
-          if h == 1 then print_string "\027[91m\u{25CF} "
-          else if h == 2 then print_string "\027[94m\u{25CF} "
+          if List.nth h row == 1 then print_string "\027[91m\u{25CF} "
+          else if List.nth h row == 2 then print_string "\027[94m\u{25CF} "
           else print_string "\u{25CF} "
         in
-        print_row t
+        print_row t row
   in
-  match board with
-  | [] -> print_string ""
-  | h :: t ->
-      let _ = print_row h in
-      print_board t
+  match row with
+  | n when n = List.length (List.hd board) -> print_string ""
+  | n ->
+      let _ = print_row board 0 in
+      print_board_grid board (n + 1)
+
+let rec print_numbers columns = function
+  | n when n = columns -> print_endline ""
+  | n ->
+      let _ = print_string (string_of_int n ^ " ") in
+      print_numbers columns (n + 1)
+
+let print_board board =
+  let _ = print_board_grid board 0 in
+  print_numbers (List.length board) 0
 
 let board = create_board 6 7
 let _ = print_board board
